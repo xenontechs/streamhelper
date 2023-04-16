466c2f8317717e1be4470917d584b5f84684c7ca
Xenontechs <xenontechs@gmail.com>
2023-04-16 03:16:20 +0200

    move initialize things to before_first_request route

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from src.routes import bp
 import logging
 import src.datastore as data
 import src.obs as obs
+import src.extensions as extensions
 
 LOG_FILENAME = "app.log"
 logging.basicConfig(filename=LOG_FILENAME, level=logging.DEBUG)
@@ -14,11 +15,19 @@ app.register_blueprint(bp)
 app.config.from_pyfile("src/flaskConfig.py")
 
 
-if __name__ == "__main__":
-    """main function"""
+@app.before_first_request
+def before_first_request():
     appconfig.testconfigfile()
     obs.prepare()
     obs.populateScenes()
+    data.config["extensions"]["exampleextension"] = "True"
+    extensions.load()
+    extensions.init()
+    appconfig.saveConfigToFile()
     data.appstatus.update()
     print("Starting server on http://localhost:5000")
+
+
+if __name__ == "__main__":
+    """main function"""
     app.run()
